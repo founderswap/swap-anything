66c7c28ec2954351722aae98af4c9882b9cfc286
ggbaro <46573388+ggbaro@users.noreply.github.com>
2023-12-31 12:14:43 +0100

    removed pydantic from core deps and expanded pandas compatibility

--- src/swapanything_backend/__init__.py
+++ src/swapanything_backend/__init__.py
@@ -1,1 +1,7 @@
+from packaging.version import Version
+from pydantic import __version__
+
 from ._base import BackendType
+
+if Version(__version__) < Version("2.0"):  # pragma: no cover
+    raise Exception("Swapanything Backend requires pydantic>=2.0")
